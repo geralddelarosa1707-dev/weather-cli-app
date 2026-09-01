311147fdf5e2866132c45e6a70f5c12a00a7137a
Gerald G. Dela Rosa <geralddelarosa1707@gmail.com>
2026-09-01 14:52:57 +0800

    Update logger.py
    Fixed the app.log current working directory

--- logger.py
+++ logger.py
@@ -1,9 +1,13 @@
 import logging
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+log_path = os.path.join(base_dir, "app.log")
 
 logger = logging.getLogger("my_cli_app")
 logger.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("app.log")
+file_handler = logging.FileHandler(log_path)
 file_handler.setLevel(logging.INFO)
 
 file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s")
